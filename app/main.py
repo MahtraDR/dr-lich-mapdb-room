0a9244ef36a7fbc1fdff4401888ffab69de9dc83
FFNG <zedwordmail@gmail.com>
2020-08-02 18:08:00 +0000

    add lame room highlight and map

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-from flask import render_template
+from flask import render_template, url_for, request
 import json
 
 app = Flask(__name__) 
@@ -17,8 +17,14 @@ def root():
 
 @app.route("/<int:room_id>")
 def room_page(room_id):
+    room_box = {'x': 0, 'y': 0, 'width': 0, 'height': 0}
     room = rd_dict.get(room_id)
+    if room.get("image_coords"):
+        room_box['x'] = room['image_coords'][0]
+        room_box['y'] = room['image_coords'][1]
+        room_box['width'] = room['image_coords'][2] - room['image_coords'][0]
+        room_box['height'] =  room['image_coords'][3] - room['image_coords'][1]
     if not room:
         return "Room not found"
-    return render_template('room.html', room=room)
+    return render_template('room.html', room=room, room_box=room_box)
     
